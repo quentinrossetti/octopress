610e37561c12b02ed740e4b13b50105993d45025
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-10-12 17:27:33 +0000

    root to /github

--- config.rb
+++ config.rb
@@ -5,10 +5,10 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/github/"
-http_images_path = "/github/images"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/github/fonts"
-css_dir = "public/github/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
